30008eafe250dc33769c55a2cdf7eb8e76b5d1c9
AlphaQUp <targetalphagone@gmail.com>
2022-03-29 17:43:12 -0600

    register.js

--- tCommands/register.js
+++ tCommands/register.js
@@ -1,5 +1,6 @@
 const DataClient = require('../packages/dataHandler.js');
-const { Client, Message } = require("discord.js")
+const { Client, Message } = require("discord.js");
+const { Console } = require('console');
 
 module.exports = {
     name: "register",
@@ -19,58 +20,56 @@ module.exports = {
                 .then(collected => {
                     collected = collected.first();
 
-                    if(args.length == 0) {
-                        if (collected.content.length >= 3) {
-                            if (collected.content.length <= 15) {
-                                if(/^[a-z0-9-]+$/i.test(collected.content)) {
-                                    const dataClientNative = new DataClient("native");
+                    if (collected.content.length >= 3) {
+                        if (collected.content.length <= 15) {
+                            if(/^[a-z0-9-]+$/i.test(collected.content)) {
+                                const dataClientNative = new DataClient("native");
 
-                                    dataClientNative.insert_one("players", {
-                                        "name": collected.content,
-                                        "id": message.author.id.toString(),
-                                        "level": 1,
-                                        "xp": 0,
-                                        "profession": "",
-                                        "class": "",
-                                        "hp": 0,
-                                        "df": 0,
-                                        "dm": 0,
-                                        "accessory": "",
-                                        "armor": "",
-                                        "weapon": "",
-                                        "inventory": [],
-                                        "gold": 0,
-                                        "realm": 0,
-                                        "adventure": 0,
-                                        "ad_block": 0,
-                                        "pass_block": 0,
-                                        "act_block_1": 0,
-                                        "act_block_2": 0
-                                    });
+                                dataClientNative.insert_one("players", {
+                                    "name": collected.content,
+                                    "id": message.author.id.toString(),
+                                    "level": 1,
+                                    "xp": 0,
+                                    "profession": "",
+                                    "class": "",
+                                    "hp": 0,
+                                    "df": 0,
+                                    "dm": 0,
+                                    "accessory": "",
+                                    "armor": "",
+                                    "weapon": "",
+                                    "inventory": [],
+                                    "gold": 0,
+                                    "realm": 0,
+                                    "adventure": 0,
+                                    "ad_block": 0,
+                                    "pass_block": 0,
+                                    "act_block_1": 0,
+                                    "act_block_2": 0
+                                });
 
-                                    message.channel.send(`Successfully registered as player \`${collected.content}\``);
-                                } else {
-                                    message.channel.send("Failed: Only characters `A-Z`, `a-z`, `0-9` and `-`.")
-                                };
+                                message.channel.send(`Successfully registered as player \`${collected.content}\``);
                             } else {
-                                message.channel.send("Failed: Cannot exceed 15 characters");
+                                message.channel.send("Failed: Only characters `A-Z`, `a-z`, `0-9` and `-`.")
                             };
                         } else {
-                            message.channel.send('Failed: Minimum of 3 characters.');
+                            message.channel.send("Failed: Cannot exceed 15 characters");
                         };
                     } else {
-                        if(['profession'].includes(args[0])) {
-                            args.shift();
-
-                            message.channel.send("Profession");
-                        } else {
-                            message.channel.send("Failed: Unknown arguments.")
-                        };
+                        message.channel.send('Failed: Minimum of 3 characters.');
                     };
                 })
                 .catch(err => {
                     message.channel.send("Timeout: Canceled operation.")
                 });
+        }  else {
+            if(['profession'].includes(args[0])) {
+                args.shift();
+
+                message.channel.send("Profession");
+            } else {
+                message.channel.send("Failed: Unknown arguments.")
+            };
         };
     }
 };
