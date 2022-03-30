e80b8b03204a4458c9559a27a9c02f084d381f74
AlphaQUp <targetalphagone@gmail.com>
2022-03-30 02:23:26 -0600

    add adventure.js

--- tCommands/adventure.js
+++ tCommands/adventure.js
@@ -16,7 +16,25 @@ module.exports = {
 
         if(args.length == 0) {
             dataClientNative.fetch_one("players", {"id":message.author.id.toString()}).then(fetch => {
-                if(fetch) {} else message.channel.send("Failed: Player not registered.");
+                if(fetch) {
+                    dataClientNative.find_one("players", {"id":message.author.id.toString()}).then(results => {
+                        if(results["adventure"] !== 0) {
+                            dataClientCore.find_one("adventures", {"id": parseInt(results["adventure"])}).then(aResults => {
+                                const elapsedTime = Math.floor((Date.now() - results["ad_block"]) / 1000);
+                                if(elapsedTime >= parseInt(aResults["time"])) {
+                                    dataClientNative.update_one("players", {"id":message.author.id.toString()}, {
+                                        $set: {
+                                            "adventure": 0,
+                                            "ad_block": 0
+                                        }
+                                    });
+
+                                    message.channel.send("Adventure completed.");
+                                } else message.channel.send(`Adventure pending. ${(parseInt(aResults["time"]) - elapsedTime)}s remaining`);
+                            });
+                        } else message.channel.send("Failed: Player not on an adventure.");
+                    });
+                } else message.channel.send("Failed: Player not registered.");
             });
         } else {
             if(['1', '2'].includes(args[0])) {
@@ -25,9 +43,13 @@ module.exports = {
                         dataClientNative.find_one("players", {"id":message.author.id.toString()}).then(results => {
                             if(results["adventure"] == 0) {
                                 dataClientNative.update_one("players", {"id":message.author.id.toString()}, {
-                                    "adventure": parseInt(args[0]),
-                                    "ad_block": Date.now()
+                                    $set: {
+                                        "adventure": parseInt(args[0]),
+                                        "ad_block": Date.now()
+                                    }
                                 });
+
+                                message.channel.send("Adventure successfully started.");
                             } else message.channel.send("Failed: Player already on an adventure.");
                         });
                     } else message.channel.send("Failed: Player not registered.");
